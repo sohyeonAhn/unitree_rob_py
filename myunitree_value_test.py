5594b11c15f3f1caa3c01fe925aacc5193e7fcf8
sohyeon910 <dksthgus777@naver.com>
2023-08-04 16:43:51 +0900

    euler,height 입력 후 동작 백업

--- myunitree_value_test.py
+++ myunitree_value_test.py
@@ -9,9 +9,7 @@ from ucl.complex import motorCmd
 class myunitree:
     def __init__(self):
         i = 0
-
     def connect(self):
-
         self.conn = unitreeConnection(HIGH_WIFI_DEFAULTS)  # 네트워크 연결
         self.conn.startRecv()
 
@@ -19,23 +17,19 @@ class myunitree:
         self.hstate = highState()
 
     def cmdInit(self):
-        time.sleep(0.5)
+        time.sleep(0.02)
         data = self.conn.getData()
         for paket in data:
             self.hstate.parseData(paket)
 
-            # self.highstate_info = f"footForce:\t\t{self.hstate.footForce}\n" \
-            #                       f"bodyHeight:\t\t{self.hstate.bodyHeight}\n" \
-            #                       f"position:\t\t{self.hstate.position}\n"
-
-            self.highstate_info = f"footForce:\t\t{self.hstate.footForce}\n" \
-                f"bodyHeight:\t\t{self.hstate.bodyHeight}\n"
+            self.highstate_info = f"bodyHeight:\t\t{self.hstate.bodyHeight}\n"
 
             self.hstate_bodyHeight = self.hstate.bodyHeight
-
             self.hstate_bms_SOC = self.hstate.bms.SOC
-
             self.hstate_footforce = self.hstate.footForce
+            self.hstate_mode =self.hstate.mode
+            # self.hstate_mode =self.hcmd.mode
+            self.hstate_gaitType =self.hstate.gaitType
 
 
     def sendCmd(self):
@@ -74,6 +68,7 @@ class myunitree:
         self.hcmd.mode = MotorModeHigh.IDLE
         self.hcmd.velocity = [0,0]  # -1  ~ +1
         self.hcmd.yawSpeed = 0
+        # self.hcmd.bodyHeight = 0
         print("STOP")
 
     def click_L(self,yawspeed_value):
@@ -81,10 +76,44 @@ class myunitree:
         self.hcmd.mode = MotorModeHigh.VEL_WALK
         self.hcmd.yawSpeed = yawspeed_value
         print("Click L")
-
     def click_R(self,yawspeed_value):
         self.cmdInit()
         self.hcmd.mode = MotorModeHigh.VEL_WALK
         self.hcmd.yawSpeed = yawspeed_value
         print("Click R")
+
+    def click_Up(self):
+        self.cmdInit()
+        self.hcmd.mode = MotorModeHigh.STAND_UP
+        print("Click Up")
+    def click_Down(self):
+        self.cmdInit()
+        self.hcmd.mode = MotorModeHigh.STAND_DOWN
+        print("Click Down")
+
+    def click_Euler(self,vel_row,vel_pitch,vel_yaw): #self,vel_row,vel_pitch,vel_yaw
+
+        self.cmdInit()
+        self.hcmd.mode = MotorModeHigh.FORCE_STAND
+
+        # euler = [Roll, Pitch, Yaw]
+        # Row: (+)왼쪽, (-)오른쪽
+        # Pitch: (+)고개 숙이기 ,(-)고개 들기
+        # Yaw: (+)왼쪽, (-)오른쪽
+        # self.hcmd.euler = [0,0,-0.1]
+        self.hcmd.euler = [vel_row,vel_pitch,vel_yaw]
+
+    def click_Height(self,vel_bodyHeight):
+        self.cmdInit()
+        self.hcmd.mode = MotorModeHigh.FORCE_STAND
+        # self.hcmd.bodyHeight = 0.1 # default: 0.28m
+        self.hcmd.bodyHeight = vel_bodyHeight # default: 0.28m
+
+    def click_Trot(self):
+        self.cmdInit()
+        self.hcmd.mode = MotorModeHigh.VEL_WALK
+        # self.hcmd.gaitType = GaitType.TROT
+        self.hcmd.gaitType = GaitType.CLIMB_STAIR
+        # # self.hcmd.velocity = [0.1, 0]  # -1  ~ +1
+        # # self.hcmd.footRaiseHeight = 0.01
 
--- save_test.py
+++ save_test.py
@@ -27,19 +27,16 @@ class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi(r'./control_gui.ui', self)
-
         self.isungb1 = myunitree()
 
-        # Create the QAction for opening the graph_gui.ui
         self.actionGraph = QAction("Open Graph", self)
         self.actionGraph.triggered.connect(self.open_graph_window)
 
-        # Add the QAction to the File menu
         self.fileMenu = self.menuBar().addMenu("Graph")
         self.fileMenu.addAction(self.actionGraph)
 
         self.connect_btn.clicked.connect(self.udp_connect)
-
+        # ------ 버튼 -----------------------------------------------------
         self.N_btn.pressed.connect(self.click_N)
         self.S_btn.pressed.connect(self.click_S)
         self.W_btn.pressed.connect(self.click_W)
@@ -47,16 +44,27 @@ class MyWindow(QMainWindow):
         self.Stop_btn.clicked.connect(self.click_Stop)
         self.L_btn.pressed.connect(self.click_L)
         self.R_btn.pressed.connect(self.click_R)
-
+        self.Up_btn.pressed.connect(self.click_Up)
+        self.Down_btn.pressed.connect(self.click_Down)
+        self.euler_btn.pressed.connect(self.click_Euler)
+        self.height_btn.pressed.connect(self.click_Height)
+        self.trot_btn.pressed.connect(self.click_Trot)
+        # ------ 값 입력 ----------------------------------------------------
         self.input_vel_0.valueChanged.connect(self.vel_0_value_changed)
         self.input_vel_1.valueChanged.connect(self.vel_1_value_changed)
         self.input_yawspeed.valueChanged.connect(self.yawspeed_value_changed)
 
+        self.input_euler_0.valueChanged.connect(self.vel_euler_value_0_changed)
+        self.input_euler_1.valueChanged.connect(self.vel_euler_value_1_changed)
+        self.input_euler_2.valueChanged.connect(self.vel_euler_value_2_changed)
+        self.input_height.valueChanged.connect(self.bodyHeight_value_changed)
+        # ------ Label -----------------------------------------------------
         self.SOC_label = self.findChild(QLabel, "SOC_label")
+        self.Mode_label = self.findChild(QLabel, "mode_label")
+        self.GaitType_label = self.findChild(QLabel, "gaittype_label")
 
     def open_graph_window(self):
         self.graph_window = myDialog(self)
-
         self.graph_window.show()
 
     def sendCmd(self):
@@ -65,12 +73,14 @@ class MyWindow(QMainWindow):
         self.highstate_textBrowser.append(self.isungb1.highstate_info)
 
         self.data_SOC = self.isungb1.hstate_bms_SOC
-        self.update_SOC_label()
+        self.data_mode = self.isungb1.hstate_mode
+        self.data_gaitType =self.isungb1.hstate_gaitType
+        self.update_label()
 
         self.plot_data_bodyHeight = self.isungb1.hstate_bodyHeight
         self.plot_data_footforce = self.isungb1.hstate_footforce
 
-
+#------데이터 입력 이벤트------------
     def vel_0_value_changed(self, value):
         self.vel_0_N = value
         self.vel_0_S = -value
@@ -81,6 +91,17 @@ class MyWindow(QMainWindow):
         self.yawspeed_value_L = value
         self.yawspeed_value_R = -value
 
+    def vel_euler_value_0_changed(self,value):
+        self.vel_euler_0 = value
+    def vel_euler_value_1_changed(self,value):
+        self.vel_euler_1 = value
+    def vel_euler_value_2_changed(self,value):
+        self.vel_euler_2 = value
+
+    def bodyHeight_value_changed(self, value):
+        self.vel_bodyheight = value
+
+#------버튼 클릭 이벤트--------------
     def click_N(self):
         self.isungb1.click_N(self.vel_0_N)
     def click_S(self):
@@ -95,7 +116,18 @@ class MyWindow(QMainWindow):
         self.isungb1.click_L(self.yawspeed_value_L)
     def click_R(self):
         self.isungb1.click_R(self.yawspeed_value_R)
-
+    def click_Up(self):
+        self.isungb1.click_Up()
+    def click_Down(self):
+        self.isungb1.click_Down()
+    def click_Euler(self):
+        self.isungb1.click_Euler(self.vel_euler_0,self.vel_euler_1,self.vel_euler_2)
+        # self.isungb1.click_Euler()
+    def click_Height(self):
+        self.isungb1.click_Height(self.vel_bodyheight)
+    def click_Trot(self):
+        self.isungb1.click_Trot()
+#----------------------------------
     def udp_connect(self):
         try:
             self.isungb1.connect()
@@ -105,9 +137,10 @@ class MyWindow(QMainWindow):
             print("udp_connect에서 예외 발생:")
             traceback.print_exc()
 
-    def update_SOC_label(self):
-
-        self.SOC_label.setText("{:.1f}%".format(self.data_SOC))  # The {:.1f} format specifies one decimal place
+    def update_label(self):
+        self.SOC_label.setText("{:.1f}".format(self.data_SOC))
+        self.Mode_label.setText("{:.1f}".format(self.data_mode))
+        self.GaitType_label.setText("{:.1f}".format(self.data_gaitType))
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
